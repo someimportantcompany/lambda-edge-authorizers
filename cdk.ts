99f1dbb36fbb85e3217569dadcf2f996b801c08a
jdrydn <james@jdrydn.com>
2024-05-10 22:53:04 +0100

    wip: Documentation, deployments & examples

--- cdk.ts
+++ cdk.ts
@@ -9,12 +9,12 @@ const app = new cdk.App();
 
 createStack(app, 'Auth0', {
   stackName: `${CDK_STACK_PREFIX}-auth0`,
-  authorizerEntryPath: path.resolve(__dirname, './packages/lambda-example-auth0/simple.ts'),
+  authorizerEntryPath: path.resolve(__dirname, './packages/lambda-example-auth0/1-out-of-the-box.ts'),
   envExamplePath: path.resolve(__dirname, './packages/lambda-example-auth0/.env.example'),
 });
 
 createStack(app, 'Standalone', {
   stackName: `${CDK_STACK_PREFIX}-standalone`,
-  authorizerEntryPath: path.resolve(__dirname, './packages/lambda-example-standalone/simple.ts'),
+  authorizerEntryPath: path.resolve(__dirname, './packages/lambda-example-standalone/1-out-of-the-box.ts'),
   authorizerIncludeBody: true,
 });
